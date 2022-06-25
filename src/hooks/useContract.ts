e59bdfcf06f90bbc7ad5b4b949f67c592e55f229
Interchained <president@worldvaporexpo.com>
2022-06-24 20:14:26 -0400

    checkpoint

--- src/hooks/useContract.ts
+++ src/hooks/useContract.ts
@@ -1,5 +1,5 @@
 import { Contract } from '@ethersproject/contracts'
-import { ChainId, WETH } from '@uniswap/sdk'
+import { ChainId } from '@uniswap/sdk'
 import { abi as IUniswapV2PairABI } from '@uniswap/v2-core/build/IUniswapV2Pair.json'
 import { useMemo } from 'react'
 import ENS_ABI from '../constants/abis/ens-registrar.json'
@@ -47,8 +47,8 @@ export function useTokenContract(tokenAddress?: string, withSignerIfPossible?: b
 }
 
 export function useWETHContract(withSignerIfPossible?: boolean): Contract | null {
-  const { chainId } = useActiveWeb3React()
-  return useContract(chainId ? WETH[chainId].address : undefined, WETH_ABI, withSignerIfPossible)
+  // const { chainId } = useActiveWeb3React()
+  return useContract('0xf93a8e5A78c7b31389b57A73f6d6770F85A06e53', WETH_ABI, withSignerIfPossible)
 }
 
 export function useENSRegistrarContract(withSignerIfPossible?: boolean): Contract | null {
@@ -87,7 +87,7 @@ export function useMulticallContract(): Contract | null {
 export function useSocksController(): Contract | null {
   const { chainId } = useActiveWeb3React()
   return useContract(
-    chainId === ChainId.MAINNET ? '0x65770b5283117639760beA3F867b69b3697a91dd' : undefined,
+    chainId === ChainId.MAINNET ? undefined : undefined,
     UNISOCKS_ABI,
     false
   )
